4078c0edc35022feea45d45237e6ef45c43061af
Joe Pettersson <joe8bit@gmail.com>
2016-02-29 17:04:54 +0000

    Fix bug in currency-parser that returned incorrect values
    It would return decimal values and not base 10 padded
    values. Doh.

--- test/currency-parser.spec.js
+++ test/currency-parser.spec.js
@@ -6,26 +6,26 @@ test('currencyParser should expose a function', t => {
 });
 
 test('currencyParser should parse dot delimited currencys', t => {
-    t.is(currencyParser('1.24'), 1.24);
+    t.is(currencyParser('1.24'), 124);
 });
 
 test('currencyParser should parse comma delimited currencys', t => {
-    t.is(currencyParser('1,24'), 1.24);
+    t.is(currencyParser('1,24'), 124);
 });
 
 test('currencyParser should parse large dot delimited currencys', t => {
-    t.is(currencyParser('12345678.24'), 12345678.24);
+    t.is(currencyParser('12345678.24'), 1234567824);
 });
 
 test('currencyParser should parse large comma delimited currencys', t => {
-    t.is(currencyParser('12,345,678.24'), 12345678.24);
+    t.is(currencyParser('12,345,678.24'), 1234567824);
 });
 
 test('currencyParser should parse dot delimited currencys with other cruft', t => {
-    t.is(currencyParser('$1.24'), 1.24);
-    t.is(currencyParser('GBP£ 1.24'), 1.24);
+    t.is(currencyParser('$1.24'), 124);
+    t.is(currencyParser('GBP£ 1.24'), 124);
 });
 
 test('currencyParser should pass through numbers', t => {
-    t.is(currencyParser(1.24), 1.24);
+    t.is(currencyParser(1.24), 124);
 });
